b1f3c67fe5414aefe1f60694a94a76494fefc529
bstadt <brandonduderstadt@gmail.com>
2016-06-12 22:03:51 -0400

    added full fade effect for landing page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,26 +1,20 @@
 $(document).ready(function(){
     $(this).scrollTop(0);
-	$('#intro').fadeIn(1500);
+	$('#intro').fadeIn(1000);
+	$('#name').delay(1100).fadeIn(1000);
+	$('#tagline').delay(1100).fadeIn(1000);
+	$('#downArrow').delay(1100).fadeIn(1000);
+	$('#nav').delay(2100).fadeIn(1000);
 });
 
-// handle links with @href started with '#' only
 $(document).on('click', 'a[href^="#"]', function(e) {
 	$('#landing').fadeIn(2000);
-    // target element id
     var id = $(this).attr('href');
-
-    // target element
     var $id = $(id);
     if ($id.length === 0) {
         return;
     }
-
-    // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
-
-    // top position relative to the document
     var pos = $(id).offset().top;
-
-    // animated top scrolling
     $('body, html').animate({scrollTop: pos}, 'slow');
 });
