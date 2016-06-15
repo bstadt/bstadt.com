9c13046af7437dd7a68e86046087e85970694477
bstadt <brandonduderstadt@gmail.com>
2016-06-14 21:43:06 -0400

    found correct backend configuration for site navigation

--- bstadt.py
+++ bstadt.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/acrobatics', methods=['GET'])
+def acrobatics():
+	if request.method == 'GET':
+		return render_template('acrobatics.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
