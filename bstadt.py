3f90f800a48c1111f58733b49c095e32e75a6e5b
bstadt <brandonduderstadt@gmail.com>
2016-06-26 11:37:53 -0400

    added files to work remotely

--- bstadt.py
+++ bstadt.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+	if request.method == 'GET':	
+		return render_template('index.html')
 
 @app.route('/acrobatics', methods=['GET'])
 def acrobatics():
 	if request.method == 'GET':
 		return render_template('acrobatics.html')
+
+
+@app.route('/projects', methods=['GET'])
+def projects():
+	if request.method == 'GET':
+		return render_template('projects.html')
+
+@app.route('/hackathons', methods=['GET'])
+def hackathons():
+	if request.method == 'GET':
+		return render_template('hackathons.html')
+
+@app.route('/circus', methods=['GET'])
+def circus():
+	if request.method == 'GET':
+		return render_template('circus.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
