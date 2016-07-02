72eb1ea7a16a0b20555bef5ac34625986b628e18
bstadt <brandonduderstadt@gmail.com>
2016-07-02 13:18:36 -0400

    finished acro page js

---
+++ static/js/acroScripts.js
@@ -0,0 +1,32 @@
+var words = $(".words");
+var i = -1;
+var first = true;
+function changeWord() {
+    i++;
+    words.eq(i%words.length).fadeIn(800).delay(1000).fadeOut(800, changeWord);
+}
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+	$('#intro').fadeIn(1000);
+	$('#name').delay(1100).fadeIn(1000);
+	$('#tagline').delay(1100).fadeIn(1000);
+	$('#downArrow').delay(1100).fadeIn(1000);
+	$('#nav').delay(2100).fadeIn(1000);
+});
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+	$('#landing').fadeIn(2000);
+    var id = $(this).attr('href');
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    e.preventDefault();
+    var pos = $(id).offset().top;
+    $('body, html').animate({scrollTop: pos}, 'slow');
+    if(first){
+        first=false;
+        changeWord();
+    }
+});
