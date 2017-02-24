4e49b4239ecf3f55a807e1cb99d21d226a84e74e
bstadt <brandonduderstadt@gmail.com>
2017-02-23 21:59:19 -0500

    fixed wierd about resize glitch

--- static/js/homeScripts.js
+++ static/js/homeScripts.js
@@ -7,14 +7,21 @@ function changeWord() {
 }
 
 $(document).ready(function(){
-    $(this).scrollTop(0);
+  $(this).scrollTop(0);
 	$('#intro').fadeIn(1000);
 	$('#name').delay(1100).fadeIn(1000);
 	$('#tagline').delay(1100).fadeIn(1000);
 	$('#downArrow').delay(1100).fadeIn(1000);
 	$('#nav').delay(2100).fadeIn(1000);
 });
-
+$(window).resize(function(){
+  if ($(window).width()<980){
+    $('#about').hide();
+  }
+  else{
+    $('#about').show();
+  }
+});
 $(document).on('click', 'a[href^="#"]', function(e) {
 	$('#landing').fadeIn(2000);
     var id = $(this).attr('href');
