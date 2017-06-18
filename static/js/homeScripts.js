7d0e9c47c9a29cf35b7e7987cd8b29221f6ac200
bstadt <brandonduderstadt@gmail.com>
2017-06-18 14:05:42 -0400

    fixed colors and added hover effects

--- static/js/homeScripts.js
+++ static/js/homeScripts.js
@@ -67,6 +67,29 @@ $(document).on('click', 'a', function(event){
     }, 1000);
 });
 
+jQuery('div.portfolio-item').hover(
+    function() {
+        jQuery(this).children('portfolio-text').show('fast');
+        return false;
+    },
+    function() {
+        jQuery(this).children('portfolio-text').hide('fast');
+        return false;
+    }
+);
+
+$('div.portfolio-item').mouseenter(function(event){
+  var original = rgb2hex($(this).css('background-color'));
+  var updated = shadeColor(original, 25);
+  $(this).css('background-color', '#' + updated);
+});
+
+$('div.portfolio-item').mouseleave(function(event){
+  var original = rgb2hex($(this).css('background-color'));
+  var updated = shadeColor(original, -25);
+  $(this).css('background-color', '#' + updated);
+});
+
 function centerDivAtLoc(xTarget, yTarget, elem) {
   var wHeight = $(window).height();
   var wWidth = $(window).width();
@@ -80,3 +103,24 @@ function centerDivAtLoc(xTarget, yTarget, elem) {
   $(elem).css("right", posX.toString()+"px");
   return;
 }
+
+function shadeColor(col, amt){
+  var num = parseInt(col,16);
+  var r = (num >> 16) + amt;
+  var b = ((num >> 8) & 0x00FF) + amt;
+  var g = (num & 0x0000FF) + amt;
+  var newColor = g | (b << 8) | (r << 16);
+  return newColor.toString(16);
+}
+
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+ }
